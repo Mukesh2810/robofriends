897137174a602062e207d412d447de14c840eeeb
Mukesh2810 <kritikauniversal28@gmail.com>
2020-07-17 10:14:53 +0530

    Updates

---
+++ precache-manifest.f9c1c37396cec13d3e5e7ad2f5abd12e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae63c9690c80bf6980378ee5ee18ac26",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "005ad378bc2817b28083",
+    "url": "/robofriends/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "58f00a035da953553ae4",
+    "url": "/robofriends/static/css/main.c45462ea.chunk.css"
+  },
+  {
+    "revision": "005ad378bc2817b28083",
+    "url": "/robofriends/static/js/2.f690e5d6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robofriends/static/js/2.f690e5d6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "58f00a035da953553ae4",
+    "url": "/robofriends/static/js/main.f98df938.chunk.js"
+  },
+  {
+    "revision": "e7d2cbb368e0813da61d",
+    "url": "/robofriends/static/js/runtime-main.74eee7b5.js"
+  }
+]);
